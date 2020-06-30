107159da5a277e4233fcb15e875307f2a5ec2441
DiegoOTdC <studioartdiego@gmail.com>
2020-06-30 21:56:17 +0200

    add LikeCounter and Reset button

--- src/components/LikeCounter.js
+++ src/components/LikeCounter.js
@@ -4,12 +4,21 @@ export default function LikeCounter() {
   const initial_numLikes = 0;
   const [numLikes, set_numLikes] = useState(initial_numLikes); // <- using state!
 
-  console.log("what are these?", numLikes, set_numLikes);
+  const increment = () => {
+    console.log("Yes, clicked! Current number of likes:", numLikes + 1);
+    set_numLikes(numLikes + 1);
+  };
+
+  const reset = () => {
+    set_numLikes(initial_numLikes);
+  };
 
   return (
     <div>
       <p>
         This post has <b>{numLikes}</b> likes!
+        <button onClick={increment}>Like</button>
+        <button onClick={reset}>Reset</button>
       </p>
     </div>
   );
