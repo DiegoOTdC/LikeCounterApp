dbbee66759e340aa1f5c75650aba5d9d8d9d2946
DiegoOTdC <studioartdiego@gmail.com>
2020-07-01 15:54:09 +0200

    add loading screen through use of ternary

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,40 +1,53 @@
 // src/components/ArticleList.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ArticleCard from "./ArticleCard";
-import Timeout from "await-timeout";
+import axios from "axios";
+import Timeout, { set } from "await-timeout";
 
 export default function ArticleList() {
-  const [articles, set_articles] = useState([
-    {
-      id: 1,
-      title: "What is React all about?",
-      body:
-        "React is all about one-way data flow, the Virtual DOM, and transpiling JSX.",
-    },
-    {
-      id: 2,
-      title: "A lovely kid",
-      body: "In fact, a kid is also the name of a baby goat!",
-    },
-    {
-      id: 3,
-      title: "On placeholder image URLs",
-      body:
-        "So yeah, you won't be able to look these images up. They're placeholders",
-    },
-  ]);
+  const [articles, set_articles] = useState();
+
+  //  useEffect(() => {
+  //    async function doSomeDataFetching() {
+  //      await Timeout.set(2000);
+  //      const response = await axios.get(
+  //        "https://jsonplaceholder.typicode.com/posts?_limit=5"
+  //      );
+  //
+  //      const { data } = response;
+  //
+  //      set_articles(data);
+  //    }
+  //    doSomeDataFetching();
+  //  }, []);
+
+  async function doSomeDataFetching() {
+    await Timeout.set(2000);
+    const url = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+    const response = await axios.get(url);
+    const { data } = response;
+    set_articles(data);
+  }
+
+  function effect() {
+    doSomeDataFetching();
+  }
+
+  useEffect(effect, []);
 
   return (
     <div>
       <p>Here's a lovely list of articles, for your reading pleasure:</p>
       <button onClick={() => set_articles([])}>Clear notifications</button>
-      {articles.map((article) => (
-        <ArticleCard
-          key={article.id}
-          title={article.title}
-          content={article.body}
-        />
-      ))}
+      {articles
+        ? articles.map((article) => (
+            <ArticleCard
+              key={article.id}
+              title={article.title}
+              content={article.body}
+            />
+          ))
+        : "Loading...."}
     </div>
   );
 }
